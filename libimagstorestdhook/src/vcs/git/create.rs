ad40c724811782e14cb3515ee957fb29ec3941fe
Matthias Beyer <mail@beyermatthias.de>
2016-05-15 00:47:56 +0200

    [PENDING] Add CreateHook

---
+++ libimagstorestdhook/src/vcs/git/create.rs
@@ -0,0 +1,40 @@
+use libimagstore::hook::Hook;
+use libimagstore::hook::position::HookPosition;
+
+struct CreateHook {
+    position: HookPosition,
+    config: Option<&Value>,
+}
+
+impl CreateHook {
+
+    pub fn new(p: HookPosition) -> CreateHook {
+        CreateHook {
+            position: p,
+        }
+    }
+
+}
+
+impl Hook for CreateHook {
+
+    fn name(&self) -> &'static str {
+        "stdhook_git_create"
+    }
+
+    fn set_config(&mut self, config: &Value) {
+        self.config = Some(config);
+    }
+
+}
+
+impl StoreIdAccessor for CreateHook {
+
+    fn access(&self, id: &StoreId) -> HookResult<()> {
+        debug!("[GIT CREATE HOOK]: {:?}", id);
+        unimplemented!()
+        Ok(())
+    }
+
+}
+
