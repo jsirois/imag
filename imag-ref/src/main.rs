ad48695cfd7a175a5c596cb8fbcf746122e9267f
Matthias Beyer <mail@beyermatthias.de>
2016-06-30 10:57:19 +0200

    [PENDING] Impl list()

--- imag-ref/src/main.rs
+++ imag-ref/src/main.rs
@@ -81,6 +81,113 @@ fn remove(rt: &Runtime) {
 }
 
 fn list(rt: &Runtime) {
+    use std::process::exit;
+
+    use libimagentrylist::lister::Lister;
+    use libimagentrylist::listers::core::CoreLister;
+
+    let cmd                      = rt.cli().subcommand_matches("list").unwrap();
+    let do_check_dead            = cmd.is_present("check-dead");
+    let do_check_changed         = cmd.is_present("check-changed");
+    let do_check_changed_content = cmd.is_present("check-changed-content");
+    let do_check_changed_permiss = cmd.is_present("check-changed-permissions");
+
+    let iter = match rt.store().retrieve_for_module("ref") {
+        Ok(iter) => iter,
+        Err(e) => {
+            trace_error(&e);
+            exit(1);
+        }
+    };
+
+    fn check_dead(r: &Ref) -> bool {
+        match r.fs_link_exists() {
+            Ok(b)  => b,
+            Err(e) => {
+                warn!("Could not check whether the ref {} exists on the FS:", r);
+                trace_error(&e);
+
+                // We continue here and tell the callee that this reference is dead, what is kind of
+                // true actually, as we might not have access to it right now
+                true
+            },
+        }
+    }
+
+    fn check_changed(r: &Ref) -> bool {
+        check_changed_content(r) && check_changed_permiss(r)
+    }
+
+    fn check_changed_content(r: &Ref) -> bool {
+        let eq = r.get_current_hash()
+            .and_then(|hash| r.get_stored_hash().map(|stored| (hash, stored)))
+            .map(|(hash, stored)| hash == stored);
+
+        match eq {
+            Ok(eq) => eq,
+            Err(e) => {
+                warn!("Could not check whether the ref {} changed on the FS:", r);
+                trace_error(&e);
+
+                // We continue here and tell the callee that this reference is unchanged
+                false
+            },
+        }
+    }
+
+    fn check_changed_permiss(r: &Ref) -> bool {
+        warn!("Permission changes tracking not supported yet.");
+        false
+    }
+
+    let lister_fn = |fle| {
+        Ref::from_filelockentry(fle) // should succeed here
+            .map(|r| {
+                let is_dead = if do_check_dead {
+                    if check_dead(&r) { "dead" } else { "alive" }
+                } else {
+                    "not checked"
+                };
+
+                let is_changed = if do_check_changed {
+                    if check_changed(&r) { "changed" } else { "unchanged" }
+                } else {
+                    "not checked"
+                };
+
+                let is_changed_content = if do_check_changed_content {
+                    if check_changed_content(&r) { "changed" } else { "unchanged" }
+                } else {
+                    "not checked"
+                };
+
+                let is_changed_permiss = if do_check_changed_permiss {
+                    if check_changed_permiss(&r) { "changed" } else { "unchanged" }
+                } else {
+                    "not checked"
+                };
+
+                format!("{} | {} | {} | {} | {} | {}",
+                        is_dead,
+                        is_changed,
+                        is_changed_content,
+                        is_changed_permiss,
+                        r.get_path_hash().unwrap_or_else(|| String::from("Cannot get hash")),
+                        r.get_location())
+            }).unwrap_or_else(|e| format!("Error: {:?}", e))
+    };
+
+    let iter = iter.filter_map(|id| {
+            match Ref::get(rt.store(), id) {
+                Ok(r) => Some(r),
+                Err(e) => {
+                    trace_error(&e);
+                    None
+                },
+            }
+        });
+
     unimplemented!()
+
 }
 
